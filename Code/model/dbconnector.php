d1e7f81192502fc3a05ac4d90ec0f9270c2e829f
Axwells <Axel.PITTET@cpnv.ch>
2023-05-09 16:49:15 +0200

    Ajout du fichier dbconnector.php de M. Nicolas Glassey

---
+++ Code/model/dbconnector.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Author   : nicolas.glassey@cpnv.ch
+ * Project  : 151_2019_ForStudents
+ * Created  : 05.02.2019 - 18:40
+ *
+ * Last update :    [01.12.2018 author]
+ *                  [add $logName in function setFullPath]
+ * Git source  :    [link]
+ *
+ *
+ * Last saved by :
+ *  Author : Axel Pittet
+ *  Project : TPI 2023 - Loc'Habitat
+ *  Date : 09.05.2023
+ *
+ */
+
+/**
+ * This function is designed to execute a query received as parameter
+ * @param $query : must be correctly build for sql (synthaxis) but the protection against sql injection will be done there
+ * @return array|null : get the query result (can be null)
+ * Source : http://php.net/manual/en/pdo.prepare.php
+ */
+function executeQuerySelect($query)
+{
+    $queryResult = null;
+
+    $dbConnexion = openDBConnexion();//open database connexion
+    if ($dbConnexion != null) {
+        $statement = $dbConnexion->prepare($query);//prepare query
+        $statement->execute();//execute query
+        $queryResult = $statement->fetchAll();//prepare result for client
+    }
+    $dbConnexion = null;//close database connexion
+    return $queryResult;
+}
+
+/**
+ * This function is designed to insert value in database
+ * @param $query
+ * @return bool|null : $statement->execute() returne true if the insert was successful
+ */
+function executeQueryIUD($query)
+{
+    $queryResult = null;
+
+    $dbConnexion = openDBConnexion();//open database connexion
+    if ($dbConnexion != null) {
+        $statement = $dbConnexion->prepare($query);//prepare query
+        $queryResult = $statement->execute();//execute query
+    }
+    $dbConnexion = null;//close database connexion
+    return $queryResult;
+}
+
+/**
+ * This function is designed to manage the database connexion. Closing will be not proceeded there. The client is responsible of this.
+ * @return PDO|null
+ * Source : http://php.net/manual/en/pdo.construct.php
+ */
+function openDBConnexion()
+{
+    $tempDbConnexion = null;
+
+    $sqlDriver = 'mysql';
+    $hostname = 'localhost';
+    $port = '3306';
+    $charset = 'utf8';
+    $dbName = 'lochabitat';
+    $userName = 'LocHabitatAdmin';
+    $userPwd = 'Pa$$w0rd';
+    $dsn = $sqlDriver . ':host=' . $hostname . ';dbname=' . $dbName . ';port=' . $port . ';charset=' . $charset;
+
+    try {
+        $tempDbConnexion = new PDO($dsn, $userName, $userPwd);
+    } catch (PDOException $exception) {
+        echo 'Connection failed: ' . $exception->getMessage();
+    }
+    return $tempDbConnexion;
+}
