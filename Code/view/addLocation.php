5bb543ab1c28ef7e0741f91e793524cee6a1ad4f
Axwells <Axel.PITTET@cpnv.ch>
2023-05-12 14:40:23 +0200

    Réalisation de la vue du formulaire d'ajout de locations

---
+++ Code/view/addLocation.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * author : Axel Pittet
+ * project : TPI 2023 - Loc'Habitat
+ * date : 12.05.2023
+ */
+
+ob_start();
+?>
+
+<div class="hero min-h-screen">
+    <div class="hero-content text-center">
+        <div class="max-w-screen-md">
+            <h1 class="text-5xl font-bold">Ajouter une location</h1>
+            <div class="divider before:bg-neutral-50 after:bg-neutral-50"></div>
+            <div class="card flex-shrink-0 w-full shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <form action="index.php?action=userLocations&userLocationsFunction=add" method="post" enctype="multipart/form-data">
+                        <div class="form-control">
+                            <input type="text" placeholder="Nom" class="input input-bordered"
+                                   name="inputLocationName" required/>
+                        </div>
+                        <br>
+                        <div class="form-control">
+                            <input type="text" placeholder="Adresse" class="input input-bordered"
+                                   name="inputLocationPlace" required/>
+                        </div>
+                        <br>
+                        <div class="form-control">
+                            <input type="text" placeholder="Description" class="input input-bordered"
+                                   name="inputLocationDescription" required/>
+                        </div>
+                        <br>
+                        <div class="form-control">
+                            <label class="label">
+                                <span class="label-text">Type d'habitation</span>
+                            </label>
+                            <div class="input input-bordered flex w-full md:items-center">
+                                <label for="maison">
+                                    <span class="label-text">Maison :</span>
+                                </label>
+                                <input id="maison" type="radio" name="inputLocationHousingType" class="radio"
+                                       checked/>
+                                <div class="divider-horizontal"></div>
+                                <label for="appartement">
+                                    <span class="label-text">Appartement :</span>
+                                </label>
+                                <input id="appartement" type="radio" name="inputLocationHousingType" class="radio"/>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="form-control">
+                            <input type="number" placeholder="Nombre maximum de clients" class="input input-bordered"
+                                   name="inputLocationClientsNb" required/>
+                        </div>
+                        <br>
+                        <div class="form-control">
+                            <input type="number" placeholder="Prix par nuit" class="input input-bordered"
+                                   name="inputLocationPrice" required/>
+                        </div>
+                        <br>
+                        <input class="file-input file-input-bordered w-full max-w-xs" type="file"
+                               name="inputLocationImage1"
+                               accept="image/*" required><br>
+                        <button type="button" id="addFile" onclick="addInput()">Ajouter un fichier</button>
+                        <div class="form-control mt-6">
+                            <input type="submit" value="Ajouter la location" class="btn btn-primary"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    function addInput() {
+        var newInput = $("<input>").attr({
+            class: "file-input file-input-bordered w-full max-w-xs",
+            type: "file",
+            name: "inputLocationImage" + ($("#myForm input[type='Image']").length + 1),
+            required: true,
+            accept: "image/*"
+        })
+
+        var deleteButton = $("<button>").attr("type", "button").text("Supprimer").on("click", function () {
+            $(this).prev().prev().remove();
+            $(this).prev().remove();
+            $(this).remove();
+        });
+
+        newInput.insertBefore($("#addFile"));
+        deleteButton.insertBefore($("#addFile"));
+        deleteButton.addClass("btn");
+        $("<br>").insertBefore($("#addFile"));
+    }
+</script>
+
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
+?>
